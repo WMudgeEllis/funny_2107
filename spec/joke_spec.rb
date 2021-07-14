0c69676744cbfd35916eb23308149790afaa4aad
Weston Ellis <ellisweston112@gmail.com>
2021-07-14 14:13:24 -0600

    Finish iteration 1

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -4,8 +4,17 @@ require './lib/joke'
 RSpec.describe do
 
   it 'exists' do
-    
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke).to be_a(Joke)
   end
 
+  it 'can read instance variables' do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke.id).to eq(22)
+    expect(joke.setup).to eq("Why did the strawberry cross the road?")
+    expect(joke.punchline).to eq("Because his mother was in a jam.")
+  end
 
 end
