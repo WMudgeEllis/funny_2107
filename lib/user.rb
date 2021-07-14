044b6045a7777384c4a258e710f43161d4276c7d
Weston Ellis <ellisweston112@gmail.com>
2021-07-14 14:42:31 -0600

    Finish iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -11,4 +11,13 @@ class User
     @jokes << joke
   end
 
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    joke_id = @jokes.select {|joke| joke.id == id}
+    joke_id.shift
+  end
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -19,6 +19,28 @@ RSpec.describe do
     expect(user_1.jokes.shift.id).to eq(22)
   end
 
+  it 'users can learn jokes from each other, and fetch jokes by id' do
+    user_1 = User.new('Sal')
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    user_1.learn(joke_1)
+    user_1.learn(joke_2)
+    user_1.tell(user_2, joke_1)
+    user_1.tell(user_2, joke_2)
+
+    expect(user_2.jokes).to be_a(Array)
+    expect(user_2.jokes.length).to eq(2)
+
+    expect(user_2.joke_by_id(22)).to be_a(Joke)
+    expect(user_2.joke_by_id(22).setup).to eq("Why did the strawberry cross the road?")
+
+    expect(user_2.joke_by_id(13)).to be_a(Joke)
+    expect(user_2.joke_by_id(13).setup).to eq("How do you keep a lion from charging?")    
+
+  end
+
 
 
 
